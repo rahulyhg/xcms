ee8ecd809d29bc61594d2c18a06524981d7f020f
Chris Herberte <chris@xweb.com.au>
2016-03-10 06:45:38 +1000

    simple theme hook for page vars

--- themes/default/page.tpl.php
+++ themes/default/page.tpl.php
@@ -1,37 +1,9 @@
 <?php
-// TODO move this into template.php
 
-/*
-$list_items = array(
-  'home' => l('Home', '<front>'),
-  'array-test' => array(
-    '1' => l('22', '<front>'),
-    '2' => l('Test', 'test'),
-    '3' => l('Test 2', 'test2'),
-  ), 'test' => l('Test', 'test'),
-  'test2' => l('Test 2', 'test2'),
-  'array-test' => array(
-    '1' => l('44', '<front>'),
-    '2' => l('Test', 'test'),
-    '3' => l('Test 2', 'test2'),
-  ),
-);
-*/
+/**
+ * @file
+ */
 
-$nav_items = array(
-  'home' => l('Home', '<front>'),
-  'test' => l('Test', 'test'),
-  'user' => l('User', 'user'),
-);
-
-if (user_is_logged_in()) {
-  $nav_items['logout'] = l('Log out', 'user/logout');
-}
-else {
-  $nav_items['login'] = l('Login', 'user/login');
-}
-
-$nav = theme_list(array('items' => $nav_items, 'attributes' => array('class' => 'nav-menu testing')));
 ?>
 
 <html>
@@ -39,18 +11,18 @@ $nav = theme_list(array('items' => $nav_items, 'attributes' => array('class' => 
     <title><?php print $head_title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="<?php print $directory; ?>/favicon.ico" type="image/vnd.microsoft.icon" />
+    <link rel="shortcut icon" href="<?php print url($directory); ?>/favicon.ico" type="image/vnd.microsoft.icon" />
     <?php print $scripts; ?>
     <?php print $styles; ?>
   </head>
-  <body>
+  <body class="<?php print $body_classes; ?>">
 
   <?php print $page_top; ?>
 
   <div id="header-wrapper">
     <div id="header" class="container clearfix">
       <div id="logo">
-        <a href="<?php print $front_url; ?>" title="Home" rel="home"><img src="<?php print $directory; ?>/logo.png" alt="Home" /></a>
+        <a href="<?php print $front_url; ?>" title="Home" rel="home"><img src="<?php print url($directory); ?>/logo.png" alt="Home" /></a>
       </div>
       <a id="mobile-nav">Menu</a>
     </div>
@@ -87,6 +59,4 @@ $nav = theme_list(array('items' => $nav_items, 'attributes' => array('class' => 
   <?php print $page_bottom; ?>
 
 </html>
-
-
 
--- themes/default/template.php
+++ themes/default/template.php
@@ -4,3 +4,30 @@
  * @file
  */
 
+/**
+ *
+ */
+function default_preprocess_page(&$vars) {
+  $vars['nav'] = _default_nav();
+}
+
+/**
+ *
+ */
+function _default_nav() {
+  $nav_items = array(
+    'home' => l('Home', '<front>'),
+    'test' => l('Test', 'test'),
+    'user' => l('User', 'user'),
+  );
+
+  if (user_is_logged_in()) {
+    $nav_items['logout'] = l('Log out', 'user/logout');
+  }
+  else {
+    $nav_items['login'] = l('Login', 'user/login');
+  }
+
+  return theme_list(array('items' => $nav_items, 'attributes' => array('class' => 'nav-menu')));
+}
+
