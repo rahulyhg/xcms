6800e84f902980b898b17722b46a2b5af85e23f4
Chris Herberte <chris@xweb.com.au>
2016-03-10 06:54:25 +1000

    comment code

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
 /**
  * @file
+ * The PHP file that serves all page requests.
+ *
+ * All XCMS code is released under the GNU General Public License.
+ * See LICENSE
  */
 
 define('ROOT_DIR', getcwd());
 require_once ROOT_DIR . '/inc/main.inc';
 xcms_init();
+
