bc0d7bd58fb16734436c50207461a623ec9ef5f2
Chris Herberte <chris@xweb.com.au>
2017-02-23 06:50:39 +1000

    gitignore

---
+++ site/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file Site settings
+ */
+
+$settings_database = array(
+  'database' => 'ck_newsite',
+  'username' => 'drupal',
+  'password' => 'drup41',
+  'host' => 'localhost',
+  'port' => '',
+);
+
+$base_path = '/';
